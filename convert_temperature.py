ac5f743af472bcf0c6247354addddc560a92467b
ngocquynh1211 <hethong1205@gmail.com>
2024-06-15 09:29:35 +0700

    [Bài tập] Chuyển đổi nhiệt độ

---
+++ convert_temperature.py
@@ -0,0 +1,8 @@
+# Nhập giá trị độ Celsius từ người dùng
+celsius = float(input("Nhập vào độ C muốn chuyển đổi: "))
+
+# Tính độ Fahrenheit tương ứng
+fahrenheit = (celsius * 1.8) + 32
+
+# Hiển thị độ Fahrenheit
+print(f"Độ F tương ứng với {celsius} °C là {fahrenheit} °F")
